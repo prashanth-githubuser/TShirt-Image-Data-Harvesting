25c5d1402be864872bbe0f9725180bbe66693f15
Prashanth M <120344718+prashanth-githubuser@users.noreply.github.com>
2024-01-24 07:21:18 +0530

    Add files via upload

--- src/Image_Data_Harvesting/config/configuration.py
+++ src/Image_Data_Harvesting/config/configuration.py
@@ -1,11 +1,30 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 @dataclass
 class Scrapper_Config:
     '''URL Works only for myntra.com domain'''
     
     URL= "https://www.myntra.com/men-tshirts"
-    page_from: int = 1
-    page_to: int = 1000
+    page_from: int = 2371
+    page_to: int = 2470
     check_point: int = 500
     csv_location: str = r'D:\Python\Test\selenium\artifacts'
+
+
+@dataclass
+class Analysis_Cleaning_Config:
+    artifact_dir: Path = Path(r'D:\Python\Test\selenium\artifacts')
+    metadata_file_dir: Path = Path(r'D:\Python\Test\selenium\artifacts\metadata')
+
+
+@dataclass
+class Image_Extraction_Config:
+    from_url: str = ''
+    from_metadata_file: str = None
+    # Enter index range if required
+    from_index: int = None
+    to_index: int = None
+    image_output_path: Path = r'D:\Python\Test\selenium\artifacts\images'
+    from_metadata: Path =  Path(r'D:\Python\Test\selenium\artifacts\metadata\cleaned_metadata')
+    
